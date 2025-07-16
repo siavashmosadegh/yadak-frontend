916ca205b39d91af141edecafcfbbbaeafb40d10
siavashmosadegh <s.mosadegh@rocketmail.com>
2025-07-16 15:11:00 +0330

    get cart items SAGA added

--- src/Redux/Checkout/Sagas.js
+++ src/Redux/Checkout/Sagas.js
@@ -47,9 +47,36 @@ function* watchIncreaseProductQuantityInCart() {
     yield takeEvery(Types.INCREASE_PRODUCT_QUANTITY_IN_CART_REQUEST, increaseProductQuantityInCart);
 }
 
+function* getCartItemsViaCartId (action) {
+    try {
+        const { cartId } = action.payload;
+
+        const result = yield call(() =>
+            fetch(`http://localhost:8080/api/v1/cart/get-cart-items-via-cart-id`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer '+ localStorage.getItem("token")
+                },
+                body: JSON.stringify({ cartId })
+            })
+            .then(res => res.json())
+        );
+        yield put({ type: Types.GET_CART_ITEMS_VIA_CART_ID_SUCCESS, payload: result });
+    } catch (error) {
+        yield put({ type: Types.GET_CART_ITEMS_VIA_CART_ID_FAIL, error });
+    }
+
+}
+
+function* watchGetCartItemsViaCartId() {
+    yield takeEvery(Types.GET_CART_ITEMS_VIA_CART_ID_REQUEST, getCartItemsViaCartId);
+}
+
 export default function* Sagas() {
-  yield all([
-    fork(watchGetCartViaUserId),
-    fork(watchIncreaseProductQuantityInCart)
-  ]);
+    yield all([
+        fork(watchGetCartViaUserId),
+        fork(watchIncreaseProductQuantityInCart),
+        fork(watchGetCartItemsViaCartId)
+    ]);
 }
