a9a4247f0b7bea9952c2698a81bcf7304e177d47
siavashmosadegh <s.mosadegh@rocketmail.com>
2025-07-26 17:56:16 +0330

    fetchProductCategories adjusted to axiosClient.js

--- src/API/productApi.js
+++ src/API/productApi.js
@@ -3,3 +3,7 @@ import axiosClient from './axiosClient.js';
 export const getProductsByIdsApi = ({ ids }) => {
     return axiosClient.post('/products/bulk', { ids });
 };
+
+export const fetchProductCategoriesApi = () => {
+    return axiosClient.get('/products/get-all-categories');
+};
--- src/Redux/General/Sagas.js
+++ src/Redux/General/Sagas.js
@@ -1,27 +1,36 @@
 import { call, put, takeEvery, all } from 'redux-saga/effects';
 import Types from './Types';
 
-function* generalSaga() {
-  console.log(">>> generalSaga started");
+import {
+    fetchProductCategoriesApi
+} from '../../API/productApi';
 
-  // Setup watcher directly with inline handler
-  yield takeEvery(Types.FETCH_PRODUCT_CATEGORIES, function* () {
-    console.log('>>> fetchProductCategories saga triggered');
+function* fetchProductCategories() {
     try {
-      const products = yield call(() =>
-        fetch('http://localhost:8080/api/v1/products/get-all-categories').then(res => res.json())
-      );
-      console.log("Fetched products:", products);
-      yield put({ type: Types.FETCH_PRODUCT_CATEGORIES_SUCCESS, payload: products });
+
+        const categories = yield call(fetchProductCategoriesApi);
+
+        yield put({
+            type: Types.FETCH_PRODUCT_CATEGORIES_SUCCESS,
+            payload: categories,
+        });
+
     } catch (error) {
-      console.error("API failed:", error);
-      yield put({ type: Types.FETCH_PRODUCT_CATEGORIES_FAIL, error });
+
+        yield put({
+            type: Types.FETCH_PRODUCT_CATEGORIES_FAIL,
+            error: error.response?.data?.message || 'Failed to fetch product categories',
+        });
+
     }
-  });
+}
+
+function* watchFetchProductCategories() {
+    yield takeEvery(Types.FETCH_PRODUCT_CATEGORIES, fetchProductCategories);
 }
 
 export default function* GeneralSagas() {
-  yield all([
-    generalSaga()
-  ]);
+    yield all([
+        watchFetchProductCategories()
+    ]);
 }
