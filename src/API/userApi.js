e74ec180f466d2e7652fcf9709b51c59ec7cc61c
siavashmosadegh <s.mosadegh@rocketmail.com>
2025-07-28 22:08:52 +0330

    get profile personal info : Saga added

--- src/API/userApi.js
+++ src/API/userApi.js
@@ -3,3 +3,7 @@ import axiosClient from './axiosClient';
 export const updateProfilePersonalInfoNamesApi = ({firstName, lastName}) => {
     return axiosClient.post('profile/personal-info/update-name', {firstName, lastName});
 };
+
+export const getProfilePersonalInfoRequestApi = () => {
+    return axiosClient.get('/profile/get-user-profile');
+};
--- src/Redux/User/Sagas.js
+++ src/Redux/User/Sagas.js
@@ -2,7 +2,8 @@ import { call, put, takeEvery, all, fork } from 'redux-saga/effects';
 import Types from './Types';
 
 import {
-    updateProfilePersonalInfoNamesApi
+    updateProfilePersonalInfoNamesApi,
+    getProfilePersonalInfoRequestApi
 } from '../../API/userApi';
 
 function* updateProfilePersonalInfoNames() {
@@ -26,8 +27,33 @@ function* watchUpdateProfilePersonalInfoNames() {
     yield takeEvery(Types.UPDATE_PROFILE_PERSONAL_INFO_NAMES_REQUEST, updateProfilePersonalInfoNames);
 }
 
+function* getProfilePersonalInfoRequest() {
+    try {
+
+        const result = yield call(getProfilePersonalInfoRequestApi);
+
+        yield put({ type: Types.GET_PROFILE_PERSONAL_INFO_SUCCESS, payload: result.result });
+
+    } catch (error) {
+
+        yield put({
+            type: Types.GET_PROFILE_PERSONAL_INFO_FAILURE,
+            error: error.response?.data?.message || 'Failed to fetch cart',
+        });
+
+    }
+}
+
+function* watchGetProfilePersonalInfoRequest() {
+
+    console.log("TRIGGERED !!!")
+
+    yield takeEvery(Types.GET_PROFILE_PERSONAL_INFO_REQUEST, getProfilePersonalInfoRequest);
+}
+
 export default function* Sagas() {
     yield all([
-        fork(watchUpdateProfilePersonalInfoNames)
+        fork(watchUpdateProfilePersonalInfoNames),
+        fork(watchGetProfilePersonalInfoRequest)
     ]);
 }
