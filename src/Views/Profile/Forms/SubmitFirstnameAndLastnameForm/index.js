b44c080298b07ad8d77067527dbd5404e1763415
siavashmosadegh <s.mosadegh@rocketmail.com>
2025-07-30 16:48:50 +0330

    SubmitFirstnameAndLastnameForm implemented ...

---
+++ src/Views/Profile/Forms/SubmitFirstnameAndLastnameForm/index.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import {
+    Form,
+    FormContainer
+} from './styles';
+import { Formik } from 'formik';
+import FormHandlers from '../../../../Util/FormHandlers';
+import CustomButton from '../../../../UI-Kit/CustomButton';
+import { SubmitFirstnameAndLastnameFormSchema } from '../../../../Validation/Profile/Schema';
+
+const SubmitFirstnameAndLastnameForm = () => {
+    return (
+        <FormContainer>
+            <Formik
+                initialValues={{
+                    firstname: "",
+                    lastname: ""
+                }}
+                validationSchema={SubmitFirstnameAndLastnameFormSchema}
+                onSubmit={(values, action) => {
+                    console.log(values);
+                }}
+            >
+                {(formik) => (
+                    <Form
+                        onSubmit={formik.handleSubmit}
+                    >
+                        <p>نام</p>
+
+                        <input
+                            name="firstname"
+                            value={formik.values.firstname}
+                            onChange={(e) => FormHandlers.onChange(e,formik)}
+                        />
+
+                        {
+                            formik.errors.firstname && formik.touched.firstname === true
+                                ?
+                                <p className='firstnameError'>
+                                    {
+                                        formik.errors.firstname
+                                    }
+                                </p>
+                                :
+                                null
+                        }
+
+                        <p style={{marginTop: "30px"}}>نام خانوادگی</p>
+
+                        <input
+                            name="lastname"
+                            value={formik.values.lastname}
+                            onChange={(e) => FormHandlers.onChange(e,formik)}
+                        />
+
+                        {
+                            formik.errors.lastname && formik.touched.lastname === true
+                                ?
+                                <p className='lastnameError'>
+                                    {
+                                        formik.errors.lastname
+                                    }
+                                </p>
+                                :
+                                null
+                        }
+
+                        <CustomButton
+                            width="100%"
+                            height="30px"
+                            title="ارسال"
+                            backgroundColor="red"
+                            borderRadius="10px"
+                            border="none"
+                            color="white"
+                            fontSize="15px"
+                            fontWeight="bold"
+                        />
+                    </Form>
+                )}
+            </Formik>
+        </FormContainer>
+    );
+}
+
+export default SubmitFirstnameAndLastnameForm;
