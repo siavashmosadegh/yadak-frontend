a5f51ecec4ebc587663b03e9e8006901ad7d512e
siavashmosadegh <s.mosadegh@rocketmail.com>
2025-07-31 14:03:47 +0330

    updateProfilePersonalInfoNames Redux Action fixed ...

--- src/Redux/User/Actions.js
+++ src/Redux/User/Actions.js
@@ -5,12 +5,9 @@ const UserActions = {
     selectAddressHandler: DelegateAction(Types.SELECT_ADDRESS)
 }
 
-export const updateProfilePersonalInfoNames = (firstName, lastName) => ({
+export const updateProfilePersonalInfoNames = (payload) => ({
     type: Types.UPDATE_PROFILE_PERSONAL_INFO_NAMES_REQUEST,
-    payload: {
-        firstName,
-        lastName
-    }
+    payload
 });
 
 export const getProfilePersonalInfoRequest = () => ({
